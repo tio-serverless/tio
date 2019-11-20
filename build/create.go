f5dbf322db7b1d0c791ee68ce46551cba96cf5bf
andy <ztao8607@gmail.com>
2019-11-20 16:19:20 +0800

    add output stream in build

--- build/create.go
+++ build/create.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 
 	docker "github.com/fsouza/go-dockerclient"
 )
 
 func buildImage(name string) error {
+	var buf bytes.Buffer
+
 	return b.DClient.BuildImage(docker.BuildImageOptions{
-		Name:       fmt.Sprintf("%s:%s", "vikings/tio-go-runtime", name),
-		Dockerfile: b.Root + "/tio/Dockerfile",
-		ContextDir: b.Root + "/tio/",
+		Name:                fmt.Sprintf("%s:%s", "vikings/tio-go-runtime", name),
+		Dockerfile:          b.Root + "/tio/Dockerfile",
+		RmTmpContainer:      true,
+		ForceRmTmpContainer: true,
+		ContextDir:          b.Root + "/tio/",
+		OutputStream:        &buf,
 	})
 }
