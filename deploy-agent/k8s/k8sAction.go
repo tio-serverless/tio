ea60e92613cc798dc2be6e6bf384b78982a31933
andy <ztao8607@gmail.com>
2019-12-17 10:50:46 +0800

    fix deployment env update failed issue

--- deploy-agent/k8s/k8sAction.go
+++ deploy-agent/k8s/k8sAction.go
@@ -14,21 +14,9 @@ func CreateNewDeploy(config *data.B, k MyK8s, s model.Server) (string, error) {
 		Image: s.Image,
 		Name:  s.Name,
 	}
-
-	isExist, err := k.IsHasDeploy(d.Name)
-	if err != nil {
-		logrus.Errorf("Check Exist Deployment Error. %s", err)
-		isExist = false
-	}
-
-	if isExist {
-		logrus.Debugf("Replace Exist Deployment[%s]", d.Name)
-		return d.Name, k.ReplaceDeploy(d)
-	}
-
 	var meta data.MyDeploy
 
-	_, err = toml.Decode(s.Raw, &meta)
+	_, err := toml.Decode(s.Raw, &meta)
 	if err != nil {
 		return "", err
 	}
@@ -43,6 +31,17 @@ func CreateNewDeploy(config *data.B, k MyK8s, s model.Server) (string, error) {
 	env["DEBUG"] = "debug"
 
 	d.Env = env
+
+	isExist, err := k.IsHasDeploy(d.Name)
+	if err != nil {
+		logrus.Errorf("Check Exist Deployment Error. %s", err)
+		isExist = false
+	}
+
+	if isExist {
+		logrus.Debugf("Replace Exist Deployment[%s]", d.Name)
+		return d.Name, k.ReplaceDeploy(d)
+	}
 
 	return k.NewDeploy(d)
 }
