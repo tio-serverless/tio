39c2e86cace2a52e302af3daf885fc3f18b6462d
andy <ztao8607@gmail.com>
2019-12-19 11:20:08 +0800

    client add set function

--- deploy-agent/k8s/k8sImplement.go
+++ deploy-agent/k8s/k8sImplement.go
@@ -178,33 +178,15 @@ func (k *SimpleK8s) ReplaceDeploy(d deploy) error {
 	}
 
 	for i, c := range oldDeployment.Spec.Template.Spec.Containers {
-		//logrus.Debugf("Find Container [%s] In [%s]", c.Name, d.Name)
-		if c.Name != "coonsul-sidecar" {
+		if c.Name != "consul-sidecar" {
 			if d.Image != "" {
 				oldDeployment.Spec.Template.Spec.Containers[i].Image = d.Image
 			}
-
-			//for n, e := range oldDeployment.Spec.Template.Spec.Containers[i].Env {
-			//	if v, ok := d.Env[e.Name]; ok {
-			//		oldDeployment.Spec.Template.Spec.Containers[i].Env[n] = apiv1.EnvVar{
-			//			Name:  e.Name,
-			//			Value: v,
-			//		}
-			//	}
-			//}
 			oldDeployment.Spec.Template.Spec.Containers[i].Env = envMerge(oldDeployment.Spec.Template.Spec.Containers[i].Env, d.Env)
 			continue
 		}
 
 		if c.Name == "consul-sidecar" {
-			//for n, e := range oldDeployment.Spec.Template.Spec.Containers[i].Env {
-			//	if v, ok := d.Env[e.Name]; ok {
-			//		oldDeployment.Spec.Template.Spec.Containers[i].Env[n] = apiv1.EnvVar{
-			//			Name:  e.Name,
-			//			Value: v,
-			//		}
-			//	}
-			//}
 			oldDeployment.Spec.Template.Spec.Containers[i].Env = envMerge(oldDeployment.Spec.Template.Spec.Containers[i].Env, d.Env)
 		}
 	}
